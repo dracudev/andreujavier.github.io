286c71b5f690eea404be228975a5afb79864d8fc
dracudev <andreujavier99@gmail.com>
2024-12-13 20:28:01 +0100

    updated info + projects in process

--- scripts/main.js
+++ scripts/main.js
@@ -1,65 +1,71 @@
-//toggle icon navbar 
+//toggle icon navbar
 let menuIcon = document.querySelector("#menu-icon");
 let navbar = document.querySelector(".navbar");
 
 menuIcon.onclick = () => {
-    menuIcon.classList.toggle("bx-x");
-    navbar.classList.toggle("active");
-}
+  menuIcon.classList.toggle("bx-x");
+  navbar.classList.toggle("active");
+};
 
 // scroll sections active link
 let sections = document.querySelectorAll("section");
 let navLinks = document.querySelectorAll("header nav a");
 
 window.onscroll = () => {
-    sections.forEach(sec => {
-        let top = window.scrollY;
-        let  offset = sec.offsetTop - 150;
-        let height = sec.offsetHeight;
-        let id = sec.getAttribute("id");
+  sections.forEach((sec) => {
+    let top = window.scrollY;
+    let offset = sec.offsetTop - 150;
+    let height = sec.offsetHeight;
+    let id = sec.getAttribute("id");
 
-        if(top >= offset && top < offset +height) {
-            navLinks.forEach(links => {
-                links.classList.remove("active");
-                document.querySelector("header nav a[href*=" + id + "]").classList.add("active");
-            })
-        }
-    })
+    if (top >= offset && top < offset + height) {
+      navLinks.forEach((links) => {
+        links.classList.remove("active");
+        document
+          .querySelector("header nav a[href*=" + id + "]")
+          .classList.add("active");
+      });
+    }
+  });
 
-// sticky navbar
-let header = document.querySelector("header");
+  // sticky navbar
+  let header = document.querySelector("header");
 
-header.classList.toggle("sticky", window.scrollY > 100);
+  header.classList.toggle("sticky", window.scrollY > 100);
 
-// remove toggle icon and navbar when click navbar link (scroll)
-menuIcon.classList.remove("bx-x");
-navbar.classList.remove("active");
-
-}
-
+  // remove toggle icon and navbar when click navbar link (scroll)
+  menuIcon.classList.remove("bx-x");
+  navbar.classList.remove("active");
+};
 
 // scroll reveal
 ScrollReveal({
-     // reset: true,
-     distance: "80px",
-     duration: 2000,
-     delay: 200
+  // reset: true,
+  distance: "80px",
+  duration: 2000,
+  delay: 200,
 });
 
-ScrollReveal().reveal('.home-content, .heading', { origin: "top" });
-ScrollReveal().reveal('.home-img, .services-container, .projects-box, .contact form', { origin: "bottom" });
-ScrollReveal().reveal('.home-content h1, #about-experience', { origin: "left" });
-ScrollReveal().reveal('.home-content p, .about-content, #about-education', { origin: "right" });
+ScrollReveal().reveal(".home-content, .heading", { origin: "top" });
+ScrollReveal().reveal(
+  ".home-img, .services-container, .projects-box, .contact form",
+  { origin: "bottom" }
+);
+ScrollReveal().reveal(".home-content h1, #about-experience", {
+  origin: "left",
+});
+ScrollReveal().reveal(".home-content p, .about-content, #about-education", {
+  origin: "right",
+});
 
 // typed js
-const typed = new Typed(".multiple-text", {
-    strings: ["Digital Marketer", "Software Developer"],
-    typeSpeed: 100,
-    backSpeed: 100,
-    backDelay: 1000,
-    loop: true   
-})
-
+// const typed = new Typed(".multiple-text", {
+//     strings: ["Digital Marketer", "Frontend Developer"],
+//     typeSpeed: 100,
+//     backSpeed: 100,
+//     backDelay: 1000,
+//     loop: true
+// })
 
 //dark/light mode
-const setTheme = theme => document.documentElement.className = theme;
+const setTheme = (theme) => (document.documentElement.className = theme);
